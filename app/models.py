5921b00d30992db0b8bb53cba618f8d58a394bd4
yovitsa <jovica04@outlook.com>
2025-01-02 19:00:04 -0800

    db finished, database intiailazied

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
 
@@ -11,6 +20,30 @@ class User(db.Model):
 
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post']= so.relationship(back_populates='author')
+
     def __repr__(self):
         return (f'User: {self.username}')
     
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+# class Cat(db.Model):
+#     id: so.Mapped[int] = so.mapped_column(primary_key=True)
+#     username: so.Mapped[str]= so.mapped_column(sa.String(32))
+
+class Post(db.Model):
+    id: so.Mapped[int]= so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(200))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return (f'Post: {self.body}')
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-
+from flask import render_template, flash, redirect, url_for, request
+from urllib.parse import urlsplit
+from app import app, db
 from app.forms import LoginForm
+from flask_login import login_required,current_user, login_user, logout_user
+import sqlalchemy as sa
+from app.models import User
+
+
+
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Jovica'}
     posts = [
@@ -22,8 +30,25 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user{form.username.data}, {form.remember_me.data}')
-        return redirect(url_for('index'))
+       user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+       if user is None or not user.check_password(form.password.data):
+           flash('Invalid username or password')
+           return redirect (url_for('login'))
+       login_user(user, remember=form.remember_me.data)
+       next_page = request.args.get('next')
+       if not next_page or urlsplit(next_page).netloc !='':
+           next_page = url_for('index')
+       return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
