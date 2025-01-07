d8f668fff5bd1ceb419fd20eb1a00f8f1ceac3c2
yovitsa <jovica04@outlook.com>
2025-01-06 19:33:04 -0800

    profile avatar does not work, need to fix it

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, BooleanField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import Length,DataRequired, ValidationError, Email, EqualTo
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -33,3 +33,10 @@ class RegistrationForm(FlaskForm):
         ))
         if user is not None:
             raise ValidationError('Please use a different email address ')
+        
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    
