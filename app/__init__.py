cfda4e9460026a358101c86ecb69086c984793ac
yovitsa <jovica04@outlook.com>
2025-01-13 20:58:54 -0800

    logger implemented, need to fix duplicate username bug

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging 
-from logging.handlers import  SMTPHandler
+from logging.handlers import  SMTPHandler, RotatingFileHandler
 
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 db =  SQLAlchemy(app)
@@ -24,11 +25,24 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject  = 'Microblog Failure',
+            toaddrs=app.config['ADMINS'], subject  = 'blog Failure',
             credentials=auth, secure=secure
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/blog.log', maxBytes=10240,backupCount=10)
+
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s %(mmessage)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Blog startup')
         
 
 
